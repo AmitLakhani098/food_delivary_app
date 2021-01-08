5a50af9835145bd3c79b433fb89b969f2a841930
amit <amitlakhani098@gmail.com>
2021-01-08 14:22:36 +0530

    splash screeen

---
+++ food_application/lib/screens/splash_screen/splash_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Splash screen"),
+      ),
+      body: Center(
+        child: Text("Splach"),
+      ),
+    );
+  }
+}
